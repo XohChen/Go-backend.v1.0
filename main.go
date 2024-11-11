211f9d6f98dedc96064b9ac5df8154bc53b79bd0
lastc0de <xohchen@gmail.com>
2024-11-11 14:18:34 +0330

    fix-review2

--- main.go
+++ main.go
@@ -24,8 +24,10 @@ func main() {
 	router.HandleFunc("/api/users", getUsers).Methods("GET")
 	router.HandleFunc("/api/users/{id}", getUser).Methods("GET")
 	router.HandleFunc("/api/users", addUser).Methods("POST")
-	router.HandleFunc("/api/users/{id}", updateUser).Methods("PUT")
-	router.HandleFunc("/api/users/{id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/api/users/update/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/api/users/removeUser/{id}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/api/users/addFr/{id}", addFreinds).Methods("POST")
+	router.HandleFunc("/api/users/deleteFr/{id}", deleteFreinds).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
@@ -121,6 +123,8 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 
 	for i, info := range infos {
 		if info.ID == id {
+			// re-view-2 -> a user should not be able to update it's id
+			updatedUser.ID = id
 			infos[i] = updatedUser
 			json.NewEncoder(w).Encode(updatedUser)
 			return
@@ -147,4 +151,82 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.NotFound(w, r)
+}
+
+// re-view2- add freindes
+func addFreinds(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	var addFreindesOfUsers Informations
+	id, errId := strconv.Atoi(params["id"])
+
+	if errId != nil {
+		http.Error(w, errId.Error(), http.StatusBadRequest)
+		return
+	}
+
+	errId = json.NewDecoder(r.Body).Decode(&addFreindesOfUsers)
+	if errId != nil {
+		http.Error(w, errId.Error(), http.StatusBadRequest)
+	}
+
+	for i, info := range infos {
+		if info.ID == id {
+			// note: should be check freinde id not exists in user freind list
+
+			//
+			addFreindesOfUsers.ID = id
+			infos[i].Freindes = append(infos[i].Freindes, addFreindesOfUsers.Freindes...)
+			json.NewEncoder(w).Encode(addFreindesOfUsers)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func deleteFreinds(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var deleteFreindesOfUsers Informations
+	err = json.NewDecoder(r.Body).Decode(&deleteFreindesOfUsers)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for j, info := range infos {
+		if info.ID == id {
+			for i := 0; i < len(deleteFreindesOfUsers.Freindes); i++ {
+				index := getIndex(info.Freindes, deleteFreindesOfUsers.Freindes[i])
+				log.Printf("%d", index)
+				deleteElement(&infos[j], index)
+			}
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+func deleteElement(me *Informations, indexOFfreindToRemove int) {
+	a := me.Freindes[:indexOFfreindToRemove]
+	b := me.Freindes[indexOFfreindToRemove+1:]
+	x := append(a, b...)
+	me.Freindes = x
+}
+
+func getIndex(arrayOfInfo []int, freindID int) int {
+	for i, arrInfo := range arrayOfInfo {
+		if arrInfo == freindID {
+			return i
+		}
+	}
+	return -1
 }
